8b7e998f2fceed8deae260efe3d65ce0a0cef7cb
infernus0520 <infernus0520@gmail.com>
2018-01-20 23:40:24 +0100

    random computer

--- src/main/java/ug/gomoku/gomoku/ui/GomokuUi.java
+++ src/main/java/ug/gomoku/gomoku/ui/GomokuUi.java
@@ -4,7 +4,10 @@ package ug.gomoku.gomoku.ui;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.*;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.UI;
+
+import java.util.Random;
 
 @SpringUI
 @Title("Gomoku")
@@ -26,21 +29,25 @@ public class GomokuUi extends UI {
                 cell = new Cell();
                 cell.setX(i);
                 cell.setY(j);
-                cell.setDescription(""+i+j);
                 cell.setHeight(50, Unit.PIXELS);
                 cell.setWidth(50, Unit.PIXELS);
                 cell.addClickListener(event -> {
-                    event.getComponent().setCaption(mark + event.getButton().getDescription());
+                    event.getComponent().setCaption(mark);
                     event.getButton().setEnabled(false);
-                    if (mark == "X") {
-                        mark = "O";
-                    } else {
-                        mark = "X";
+                    if(chechWin(gridToTab(gridLayout),mark)) {
+                        gridLayout.setEnabled(false);
                     }
-                });
-                gridLayout.addComponent(cell, i, j);
 
-                gridLayout.setEnabled(false);
+                    randomComputerMove(gridLayout);
+
+                    if(chechWin(gridToTab(gridLayout),"O")) {
+                        gridLayout.setEnabled(false);
+                    }
+
+                });
+
+
+                gridLayout.addComponent(cell, i, j);
 
             }
         }
@@ -50,6 +57,96 @@ public class GomokuUi extends UI {
 
 
 
+
 	}
+
+	boolean chechWin(String[][] tab,String mark) {
+
+        for (i = 0 ; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                if(j+4 < 10) {
+                    if (tab[i][j] == mark && tab[i][j+1] == mark && tab[i][j+2] == mark && tab[i][j+3] == mark && tab[i][j+4] == mark) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        for (i = 0 ; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                if(i+4 < 10) {
+                    if (tab[i][j] == mark && tab[i+1][j] == mark && tab[i+2][j] == mark && tab[i+3][j] == mark && tab[i+4][j] == mark) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        for (i = 0 ; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                if(j+4 < 10 && i+4 < 10) {
+                    if (tab[i][j] == mark && tab[i+1][j+1] == mark && tab[i+2][j+2] == mark && tab[i+3][j+3] == mark && tab[i+4][j+4] == mark) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        for (i = 0 ; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                if(i+4 < 10 && j-4 >= 0) {
+                    if (tab[i][j] == mark && tab[i+1][j-1] == mark && tab[i+2][j-2] == mark && tab[i+3][j-3] == mark && tab[i+4][j-4] == mark) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+
+	    return false;
+    }
+
+    void move(GridLayout gridLayout,int i, int j) {
+	    gridLayout.getComponent(i,j).setCaption("O");
+    }
+
+    void randomComputerMove(GridLayout gridLayout) {
+        Random randomGenerator = new Random();
+	    int i;
+	    int j;
+	    boolean t = true;
+	    while (t == true) {
+            i = randomGenerator.nextInt(10);
+            j = randomGenerator.nextInt(10);
+
+            if(gridLayout.getComponent(i,j).getCaption() != "X" && gridLayout.getComponent(i,j).getCaption() != "O" ) {
+                t = false;
+                move(gridLayout,i,j);
+                gridLayout.getComponent(i,j).setEnabled(false);
+            }
+        }
+
+
+    }
+
+    String[][] gridToTab(GridLayout gridLayout) {
+	    String[][] tab = new String[10][10];
+
+        for (i = 0 ; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                if(gridLayout.getComponent(i,j).getCaption() == "X") {
+                    tab[i][j] = "X";
+                } else if (gridLayout.getComponent(i,j).getCaption() == "O") {
+                    tab[i][j] = "O";
+                } else {
+                    tab[i][j] = "";
+                }
+            }
+        }
+
+        return tab;
+    }
+
+
 
 }
